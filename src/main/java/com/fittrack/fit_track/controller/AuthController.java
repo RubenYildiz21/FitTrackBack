1699457f05aa25af4544d42b781aeff16338fbe0
Bouzia Badr <b.bouzia@student.helmo.be>
2024-10-28 16:35:51 +0100

    correction login

--- src/main/java/com/fittrack/fit_track/controller/AuthController.java
+++ src/main/java/com/fittrack/fit_track/controller/AuthController.java
@@ -1,8 +1,12 @@
 package com.fittrack.fit_track.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,22 +29,25 @@ public class AuthController {
     private PasswordEncoder passwordEncoder;
 
     @PostMapping("/login")
-    public String login(@RequestParam String email, @RequestParam String password) {
-
+    public ResponseEntity<Map<String, String>> login(@RequestParam String email, @RequestParam String password) {
         Optional<User> userOptional = userRepository.findByEmail(email);
+        Map<String, String> response = new HashMap<>();
 
-        if (userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             User user = userOptional.get();
-
-            if(passwordEncoder.matches(password, user.getPassword())){
-                return "Connexion succes";
-            }else{
-                return "Password wrong";
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                response.put("message", "Connexion succes");
+                return ResponseEntity.ok(response);
+            } else {
+                response.put("message", "Password wrong");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
             }
-        }else{
-            return "User not found";
+        } else {
+            response.put("message", "User not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
-    
-    
+
+
+
 }
