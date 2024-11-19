878567746f525f4e6770c8cc69fc791baf0d487b
Q210220 <m.willems@student.helmo.be>
2024-11-19 13:57:36 +0100

    challenge v1

---
+++ src/main/java/com/fittrack/fit_track/model/Challenge.java
@@ -0,0 +1,66 @@
+package com.fittrack.fit_track.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Challenge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long idUser;
+    private String title;
+    private String exercise;
+    private LocalDate beginingDate;
+    private LocalDate endingDate;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Long idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(String exercise) {
+        this.exercise = exercise;
+    }
+
+    public LocalDate getBeginingDate() {
+        return beginingDate;
+    }
+
+    public void setBeginingDate(LocalDate beginingDate) {
+        this.beginingDate = beginingDate;
+    }
+
+    public LocalDate getEndingDate() {
+        return endingDate;
+    }
+
+    public void setEndingDate(LocalDate endingDate) {
+        this.endingDate = endingDate;
+    }
+}
